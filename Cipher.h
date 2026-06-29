658fc5c2e0442fb984f99dcb633c0164f6e50509
daratarasevic <tarasevish80@gmail.com>
2026-06-30 00:23:23 +0300

    transfer cipher.h logic to this new file from previous assignment

---
+++ Cipher.h
@@ -0,0 +1,29 @@
+#ifndef ASSIGNMENT_4_CIPHER_H
+#define ASSIGNMENT_4_CIPHER_H
+
+#include <string>
+
+class Cipher {
+public:
+    virtual std::string encrypt(const std::string& text) = 0;
+    virtual std::string decrypt(const std::string& text) = 0;
+    virtual ~Cipher() = default;
+};
+
+class CaesarCipher : public Cipher {
+    int key_;
+public:
+    CaesarCipher(int key);
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
+
+class VigenereCipher : public Cipher {
+    std::string key_;
+public:
+    VigenereCipher(const std::string& key);
+    std::string encrypt(const std::string& text) override;
+    std::string decrypt(const std::string& text) override;
+};
+
+#endif //ASSIGNMENT_4_CIPHER_H
