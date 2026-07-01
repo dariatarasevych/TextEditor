c8d91072cadd77a905cd27d697d9b96fb8d3a25d
daratarasevic <tarasevish80@gmail.com>
2026-07-01 13:17:59 +0300

    changed Cipher.h to make it suitable for dll

--- Cipher.h
+++ Cipher.h
@@ -4,26 +4,24 @@
 #include <string>
 
 class Cipher {
-public:
-    virtual std::string encrypt(const std::string& text) = 0;
-    virtual std::string decrypt(const std::string& text) = 0;
-    virtual ~Cipher() = default;
-};
+private:
+    int type;
+    int caesar_key;
+    std::string vigenere_key;
+    std::string lib_path;
 
-class CaesarCipher : public Cipher {
-    int key_;
-public:
-    CaesarCipher(int key);
-    std::string encrypt(const std::string& text) override;
-    std::string decrypt(const std::string& text) override;
-};
+    typedef void* cipher_t;
 
-class VigenereCipher : public Cipher {
-    std::string key_;
+    typedef cipher_t* (*CreateCaesarFunc)(int);
+    typedef cipher_t* (CreateVigenereFunc)(const char*);
+    typedef char* (*EncryptFunc)(cipher_t*, const char*);
+    typedef char* (*DecryptFunc)(cipher_t*, const char*);
+    typedef void (*DestroyFunc)(cipher_t*);
+    typedef void (*FreeStrFunc)(char*);
 public:
-    VigenereCipher(const std::string& key);
-    std::string encrypt(const std::string& text) override;
-    std::string decrypt(const std::string& text) override;
-};
+    Cipher(int t, int c_key, const std::string& v_key, const std::string& path = "./libcipher.dylib") : type(t), caesar_key(c_key), vigenere_key(v_key), lib_path(path) {}
 
+    std::string encrypt(const std::string& text);
+    std::string decrypt(const std::string& text);
+};
 #endif //ASSIGNMENT_4_CIPHER_H
